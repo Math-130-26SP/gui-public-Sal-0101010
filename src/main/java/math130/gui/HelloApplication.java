6ab49d6d080692b4b5514c217e1c2648a67cc69c
Sal-0101010 <sg1192121@swccd.edu>
2026-04-29 12:32:29 -0700

    Update HelloApplication.java

--- src/main/java/math130/gui/HelloApplication.java
+++ src/main/java/math130/gui/HelloApplication.java
@@ -1,53 +1,19 @@
 package math130.gui;
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.event.ActionEvent;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.scene.Node;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+
 import java.io.IOException;
 
-public class HelloController implements Initializable {
-
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    private Label Label1;
-
-    @FXML
-    private Spinner<Integer> players;
-
-    @FXML
-    private Button playButton;
-
-    // This runs automatically when the first screen opens
+public class HelloApplication extends Application {
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        // Set up your spinner here
-        SpinnerValueFactory<Integer> spinFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 4, 1);
-        players.setValueFactory(spinFactory);
-    }
-
-    @FXML
-    private void handleButtonClick(ActionEvent event) {
-        try {
-            // Loading the second view
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("second-view.fxml"));
-            Parent root = loader.load();
-
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (IOException e) {
-            System.err.println("Could not find second-view.fxml. Check the filename!");
-            e.printStackTrace();
-        }
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
